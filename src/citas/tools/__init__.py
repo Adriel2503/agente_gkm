ed9bb9e37ebe7dfa787cd849fae96406d63db251
Dante <ariel.frias.r@uni.pe>
2026-03-16 11:59:00 -0500

    Parche a outpur parser

--- src/citas/tools/__init__.py
+++ src/citas/tools/__init__.py
@@ -2,6 +2,6 @@
 Tools internas del agente de citas.
 """
 
-from .tools import AGENT_TOOLS, check_availability, create_booking, search_productos_servicios
+from .tools import AGENT_TOOLS, check_availability, create_booking, search_productos_servicios, search_kia_modelos
 
-__all__ = ["AGENT_TOOLS", "check_availability", "create_booking", "search_productos_servicios"]
+__all__ = ["AGENT_TOOLS", "check_availability", "create_booking", "search_productos_servicios", "search_kia_modelos"]
